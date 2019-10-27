35e71d2e31142e815ff91395769f47715e02708e
bymathears <57061484+bymathears@users.noreply.github.com>
2019-10-27 13:54:50 +0100

    Add files via upload

---
+++ igMods/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='igMod',
+      version='0.1',
+      description='igMods bc4',
+      author='bc4',
+      packages=['IGMods'],
+      install_requires=["instapy",],
+      zip_safe=False)
